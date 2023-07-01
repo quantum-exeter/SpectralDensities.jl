2e80206573212b1a39573c79fde2fb970da346db
Documenter.jl <documenter@juliadocs.github.io>
2023-07-01 15:29:43 +0000

    build based on 16e392e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SpectralDensities","category":"page"},{"location":"#SpectralDensities","page":"Home","title":"SpectralDensities","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SpectralDensities.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SpectralDensities]","category":"page"}]
+}
